35b3956982c0cac4d53464ed61e69e7a777f7e51
ktok0820 <30647168+ktok0820@users.noreply.github.com>
2017-08-02 15:41:36 +0900

    Add files via upload

---
+++ test.c
@@ -0,0 +1,36 @@
+
+#include "stdio.h"
+#include "stdlib.h"
+
+#define MEM_ALLOC_ERROR -1
+#define OK 0
+#define SIZE_ERROR -2
+#define OTHER_ERROR -3
+
+int path_test(int size) {
+  char *p;
+  
+  if (size <=0 )
+  	return SIZE_ERROR;
+  
+  p = malloc(size);
+
+  if (!p)
+    return MEM_ALLOC_ERROR;
+
+  if (!some_other_function()) {
+    free(p);
+    return OTHER_ERROR;
+  }
+
+  if (!yet_another_function()) {
+    return OTHER_ERROR;
+  }
+
+  do_some_things(p);
+
+  free(p);
+  return OK;
+}
+
+
